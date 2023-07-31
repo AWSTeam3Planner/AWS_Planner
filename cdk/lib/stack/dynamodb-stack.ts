e886f8402855903b9d73ac86c761941db03d1d22
minjo-on <103498687+minjo-on@users.noreply.github.com>
2023-07-31 13:50:38 +0900

    cdk 구성완료

---
+++ cdk/lib/stack/dynamodb-stack.ts
@@ -0,0 +1,27 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { Stack, StackProps, Duration} from 'aws-cdk-lib';
+import { Table, AttributeType,BillingMode} from 'aws-cdk-lib/aws-dynamodb';
+
+export class PlannerDynamoDBStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // DynamoDB 테이블 생성
+    const dataTable = new Table(this, 'PlannerData', {
+        tableName : 'PlannerData',
+        partitionKey: { name: 'ID', type: AttributeType.STRING },
+        sortKey: { name: 'DATE', type: AttributeType.STRING },
+        billingMode: BillingMode.PAY_PER_REQUEST,
+        removalPolicy: cdk.RemovalPolicy.DESTROY, // 스택 삭제시 테이블도 함께 삭제합니다.
+    });
+
+    const userTable = new Table(this, 'PlannerUser', {
+        tableName : 'PlannerUser',
+        partitionKey: { name: 'ID', type: AttributeType.STRING },
+        billingMode: BillingMode.PAY_PER_REQUEST, // 또는 PAY_PER_REQUEST
+        removalPolicy: cdk.RemovalPolicy.DESTROY, // 스택 삭제시 테이블도 함께 삭제합니다.
+      });
+      dataTable.node.addDependency(userTable)
+  }
+}
