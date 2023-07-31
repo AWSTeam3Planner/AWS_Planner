f336b5fdc1ea47848c18bc9849198bb281da9f51
Jinseo Jeong <104565869+JinNitt@users.noreply.github.com>
2023-07-31 15:31:50 +0900

    [FEAT] Plan CRUD.py
    CRUD 기능 파이썬 코드 작성

---
+++ cdk/planPythonCode/plan_delete.py
@@ -0,0 +1,19 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('PlannerData')  # 여기에 테이블 이름을 적절히 변경해야 합니다.
+
+def lambda_handler(event, context):
+    try:
+        event_id = event['queryStringParameters']['event_id']
+        response = table.delete_item(Key = {'event_id': event_id})
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Event deleted successfully!')
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error deleting event: ' + str(e))
+        }
