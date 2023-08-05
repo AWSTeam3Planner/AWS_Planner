9c4ee6a529b2fe069ad5e0acb7384dcf703d3c3f
minjo-on <103498687+minjo-on@users.noreply.github.com>
2023-08-06 06:34:04 +0900

    [FEAT] cdk 스택 추가
    api gateway, cognito 추가

--- cdk/lib/planner-stack.ts
+++ cdk/lib/planner-stack.ts
@@ -3,6 +3,8 @@ import { Construct } from 'constructs';
 import {PlannerLambdaStack} from './stack/lambda-stack'
 import {PlannerS3Stack} from './stack/s3-stack'
 import {PlannerDynamoDBStack} from './stack/dynamodb-stack'
+import {PlannerAPIgatewayStack} from './stack/apigateway-stack'
+import {PlannerCognitoStack} from './stack/cognito-stack'
 import { SYSTEM_NAME } from './config/commons';
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -15,5 +17,7 @@ export class PlannerStack extends cdk.Stack {
     new PlannerLambdaStack(this, `${SYSTEM_NAME}-lambdaStack`,props);
     new PlannerDynamoDBStack(this, `${SYSTEM_NAME}-dynamodbStack`,props);
     new PlannerS3Stack(this, `${SYSTEM_NAME}-s3Stack`,props);
+    new PlannerAPIgatewayStack(this,`${SYSTEM_NAME}-APIgatewayStack`,props);
+    new PlannerCognitoStack(this,`${SYSTEM_NAME}-cognitoStack`,props);
   }
 }
